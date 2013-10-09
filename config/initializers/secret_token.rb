a7788ce9c5fa6ef580a88758b217066bde6927cc
Apprentice <apprentice@dbc-21.local>
2013-10-09 10:33:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snackoverflow::Application.config.secret_key_base = 'c4139d76ca6cb614b6f29e46cd9a969775d7d3f11a53f571678e90f05856cd62179efd16075914ef1119869da6fea27ddca0180607881ea161a0389fa9045061'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snackoverflow::Application.config.session_store :cookie_store, key: '_snackoverflow_session'
